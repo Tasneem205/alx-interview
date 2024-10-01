60d22cc22cacdcd8ad5bd052063a11f9a9685ad2
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-01 20:22:29 +0300

    pascal triangle update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,16 +6,16 @@ define pascal triangle in a string format
 
 def pascal_triangle(n):
     tri = []
-    str1 = "1"
-    str2 = ""
+    str1 = [1]
+    str2 = []
     tri.append(str1)
     for _ in range(1, n):
         for i in range(0, len(str1) + 1):
             if i == 0 or i == len(str1):
-                str2 += str1[0]
+                str2.append(str1[0])
             else:
-                str2 += str(int(str1[i]) + int(str1[i-1]))
+                str2.append(str1[i] + str1[i-1])
         tri.append(str2)
         str1 = str2
-        str2 = ""
+        str2 = []
     return tri
