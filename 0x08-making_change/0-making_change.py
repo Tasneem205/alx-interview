a9e8b7ed0852510d716fb9774ce7791e37b4f1d8
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-11-29 04:17:59 +0200

    0x08 making change 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,28 +4,18 @@ making change function file
 """
 
 
-def countRecur(coins, n, sum, memo):
-    """Find the minimum number of coins and use memoization."""
-    if sum == 0:
-        return 0
-    if sum < 0 or n == 0:
-        return float('inf')
-
-    if memo[n - 1][sum] != -1:
-        return memo[n - 1][sum]
-
-    include = 1 + countRecur(coins, n, sum - coins[n - 1], memo)
-    exclude = countRecur(coins, n - 1, sum, memo)
-    memo[n - 1][sum] = min(include, exclude)
-    return memo[n - 1][sum]
-
-
 def makeChange(coins, total):
     """Find the minimum number of coins needed to make the total."""
     if total <= 0:
         return 0
 
-    memo = [[-1 for _ in range(total + 1)] for _ in range(len(coins))]
-    result = countRecur(coins, len(coins), total, memo)
+    coins = list(set(coins))
 
-    return result if result != float('inf') else -1
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
