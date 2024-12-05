972558e008f393c0f2f17c152fc1172c692c9755
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-05 07:27:28 +0200

    fix algorithm style

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,19 +5,15 @@
 def island_perimeter(grid):
     """function to cal island perimeter"""
     peri = 0
-    for i in range(0,len(grid)):
-        for j in range(0, len(grid[i])):
-            if i == 0 and grid[i][j] == 1:
-                peri += 1
-            elif i == len(grid)-1 and grid[i][j] == 1:
-                peri += 1
-            elif grid[i-1][j] != grid[i][j]:
-                peri += 1
-
-            if j == 0 and grid[i][j] == 1:
-                peri += 1
-            elif j == len(grid[i])-1 and grid[i][j] == 1:
-                peri += 1
-            elif grid[i][j-1] != grid[i][j]:
-                peri += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    peri += 1
+                if i == len(grid)-1 or grid[i+1][j] == 0:
+                    peri += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    peri += 1
+                if j == len(grid[i])-1 or grid[i][j+1] == 0:
+                    peri += 1
     return peri
